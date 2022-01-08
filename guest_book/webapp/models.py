e64fd91d047c7e1d38012239056046ae1b4fd01b
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-08 11:47:03 +0600

    craete model Customer in models.py simple register it in admin.py

--- guest_book/webapp/models.py
+++ guest_book/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+STATUSCHOICES = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=100, null=False, blank=False, verbose_name="name")
+    customer_email = models.EmailField(max_length=254, null=False, blank=False, verbose_name="email")
+    order_text = models.TextField(max_length=2000, null=False, blank=False, verbose_name="order_text")
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=100, null=False, blank=False, verbose_name='статус',
+                              choices=STATUSCHOICES, default='active')
