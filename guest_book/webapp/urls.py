367ddee0997508a9db9a03e3033ce6ec5fd1dac6
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-08 14:15:40 +0600

    add create_order.html and respectively view and urls path to that to work

--- guest_book/webapp/urls.py
+++ guest_book/webapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, create_view
 
 urlpatterns = [
     path('', index_view, name='index_view'),
+    path('create/', create_view, name='create_view'),
+
 ]
--- guest_book/webapp/views.py
+++ guest_book/webapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import CustomerForm
 from .models import Customer
 
 # Create your views here.
@@ -8,8 +9,22 @@ from .models import Customer
 def index_view(request):
     return render(request, 'index.html', {'context': Customer.objects.all().order_by('-create_time')})
 
+
 def create_view(request):
-    pass
+    if request.method == 'GET':
+        form = CustomerForm()
+        return render(request, "create_order.html", {'form': form})
+    else:
+        form = CustomerForm(data=request.POST)
+        if form.is_valid():
+            customer_name = request.POST.get('customer_name')
+            customer_email = request.POST.get('customer_email')
+            order_text = request.POST.get('order_text')
+            order = Customer(customer_name=customer_name, customer_email=customer_email,
+                             order_text=order_text)
+            order.save()
+            return redirect('index_view')
+        return render(request, "create_order.html", {'form': form})
 
 def update_view(request):
     pass
