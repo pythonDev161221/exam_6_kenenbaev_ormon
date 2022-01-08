e6504a7c34c333e29a3d9c6f3cb5d8ba8a41cb8c
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-08 12:22:17 +0600

    create row index.html and respectively ursl.py with path and index_view in views.py

--- guest_book/webapp/views.py
+++ guest_book/webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Customer
+
 # Create your views here.
+
+
+def index_view(request):
+    return render(request, 'index.html', {'context': Customer.objects.all()})
